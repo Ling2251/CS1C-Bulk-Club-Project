701ec622cbed2b0bc35259aa7b436826cb6e3c60
Peijun Zhao <pzhao1@saddleback.edu>
2022-12-04 20:24:20 -0800

    Added more on Adminpage

--- adminpage.cpp
+++ adminpage.cpp
@@ -12,3 +12,27 @@ adminpage::~adminpage()
 {
     delete ui;
 }
+
+void adminpage::on_AddOrDeleteItemButton_clicked()
+{
+    addItemUi.show();
+}
+
+
+void adminpage::on_CreatePurchaseButton_clicked()
+{
+    memberPurchaseUi.show();
+}
+
+
+void adminpage::on_AddOrDeleteCustomerButton_clicked()
+{
+    addCustomerUi.show();
+}
+
+
+void adminpage::on_ChangeMemberShipButton_clicked()
+{
+    membershipChangeUi.show();
+}
+
--- adminpage.h
+++ adminpage.h
@@ -2,6 +2,11 @@
 #define ADMINPAGE_H
 
 #include <QDialog>
+#include "addcustomer.h"
+#include "additme.h"
+#include "dbmanager.h"
+#include "memberpurchase.h"
+#include "membershipchange.h"
 
 namespace Ui {
 class adminpage;
@@ -15,8 +20,23 @@ public:
     explicit adminpage(QWidget *parent = nullptr);
     ~adminpage();
 
+private slots:
+    // all the of the button functionality is to change between different UI
+    void on_AddOrDeleteItemButton_clicked();
+
+    void on_CreatePurchaseButton_clicked();
+
+    void on_AddOrDeleteCustomerButton_clicked();
+
+    void on_ChangeMemberShipButton_clicked();
+
 private:
     Ui::adminpage *ui;
+    addItme addItemUi;
+    addCustomer addCustomerUi;
+    memberPurchase memberPurchaseUi;
+    membershipChange membershipChangeUi;
+
 };
 
 #endif // ADMINPAGE_H
