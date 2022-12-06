3037059ebe231e625a332681d1f4c669a2546e71
Peijun Zhao <pzhao1@saddleback.edu>
2022-12-05 16:45:35 -0800

    rewrite the dbmanager class

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -3,8 +3,6 @@
 DBManager::DBManager()
 {
     // Connecting to database
-    QSqlDatabase m_database = QSqlDatabase::addDatabase("QSQLITE");
-    m_database.setDatabaseName("../CS1C-Bulk-Club-Project/Database.db");
     if(!m_database.open())
     {
        qDebug() << "Error: connection with database failed";
@@ -14,27 +12,9 @@ DBManager::DBManager()
     }
 }
 
-DBManager::~DBManager(){
-    //close database
-    m_database.close();
-    qDebug() << "database connection closed\n";
-}
+DBManager::~DBManager(){}
 
-QSqlQueryModel *DBManager::loadEntries()
-{
-    QSqlQueryModel* model = new QSqlQueryModel();
 
-    QSqlQuery qry;
-    qry.prepare("select name, type, dailySalesReport.ID, purchaseDate, item, price, quantity "
-                "from Customers, dailySalesReport where Customers.ID = dailySalesReport.ID;");
 
-    if(!qry.exec())
-    {
-        qDebug() <<"error Loading values to db" << endl;
 
-    }
-    model->setQuery(qry);
-
-    return model;
-}
 
--- dbmanager.h
+++ dbmanager.h
@@ -11,10 +11,29 @@ public:
     DBManager();
     ~DBManager();
 
-    // different functions
-    QSqlQueryModel *loadEntries();
-private:
     QSqlDatabase m_database;
+    // close the data base connection and will remove the data base to a defual connection
+    void connClose(){
+        m_database.close();
+        m_database.removeDatabase(QSqlDatabase::defaultConnection);
+    };
+
+    // opened the data base here so other cpp/h can have accses to it
+    bool connOpend(){
+        m_database = QSqlDatabase::addDatabase("QSQLITE");
+        m_database.setDatabaseName("../CS1C-Bulk-Club-Project/Database.db");
+        if(!m_database.open())
+        {
+           qDebug() << "Error: connection with database failed";
+           return false;
+        }
+        else{
+           qDebug() << "Database: connection ok";
+           return true;
+        }
+    };
+
+private:
 };
 
 #endif // DBMANAGER_H
